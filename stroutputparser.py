5bb5628990f81faf1ffff980ecef0cb0821fa962
uneiz1103 <uneizshaikh1103@gmail.com>
2025-07-27 23:06:15 +0530

    add stroutputparser.py

--- stroutputparser.py
+++ stroutputparser.py
@@ -1,1 +1,32 @@
- 
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task='text-generation'
+)
+
+model = ChatHuggingFace(llm = llm)
+
+# 1st template
+template1 = PromptTemplate(
+    template='Write a detailed report on {topic}',
+    input_variables=['topic']
+)
+
+# 2nd template
+template2 = PromptTemplate(
+    template='Write a 5 line summary on the following text. /n {text}',
+    input_variables=['text']
+)
+
+prompt1 = template1.invoke({'topic': 'black hole'})
+
+result = model.invoke(prompt1)
+
+prompt2 = template2.invoke({'text': result.content})
+
+result = model.invoke(prompt2)
